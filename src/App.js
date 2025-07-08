958cb7f35e5a2fa705d5e31aa7fb5cd4dbae2834
connor-milloy <connor.milloy@skyscanner.net>
2025-07-08 14:41:50 +0100

    menu logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import STYLES from './App.module.scss';
 import useRandomCard from './hooks/useRandomCard';
 import Card from './components/Card';
+import { useState } from 'react';
 
 const App = () => {
+  const [firstLoad, setFirstLoad] = useState(true);
+  const [gameActive, setGameActive] = useState(false);
   const { card, loading, error } = useRandomCard();
+
+  if (firstLoad) {
+    return (
+      <div className={STYLES.App}>
+        <h1>What's that Glimmer?</h1>
+        <p>How well do you know your Lorcana cards? Click start to find out.</p>
+        <button
+          className={STYLES.StartButton}
+          onClick={() => {
+            setFirstLoad(false);
+            setGameActive(true);
+          }}
+        >
+          Start Game
+        </button>
+      </div>
+    );
+  }
+
   if (loading)
     return (
       <div className={STYLES.App}>
@@ -21,7 +43,6 @@ const App = () => {
     );
   }
 
-  console.log(card);
   return (
     <div className={STYLES.App}>
       <Card card={card} />
